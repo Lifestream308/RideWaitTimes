829598f8fdea37102190568fa8dcf079e42ad845
Christian Arroyo <ChristianArroyo308@yahoo.com>
2025-03-12 01:17:38 -0700

    trying to connect to firebase. needs more work.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { db } from './firebase-config'
+import {doc, getDoc} from 'firebase/firestore'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import HomePageComponent from './components/HomePageComponent'
@@ -6,6 +9,33 @@ import AboutComponent from './components/AboutComponent'
 import ContactComponent from './components/ContactComponent'
 
 function App() {
+
+  const getTodaysDocument = async () => {
+
+    const today = String(new Date().toLocaleDateString("en-CA", { timeZone: "America/Los_Angeles" }));
+    console.log(today)
+
+    try {
+      const docRef = doc(db, "waitTimes", "2025-03-11");
+      const docSnap = await getDoc(docRef);
+  
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+        return docSnap.data();
+      } else {
+        console.log("No such document!");
+        return null;
+      }
+    } catch (error) {
+      console.error("Error retrieving document:", error);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    getTodaysDocument()
+  }, [])
+  
 
   return (
     <>
