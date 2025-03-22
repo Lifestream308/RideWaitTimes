4d33d5f0a32bb4e8e206444622fae801dbd6ceaf
Christian Arroyo <ChristianArroyo308@yahoo.com>
2025-03-22 00:46:39 -0700

    added Disney logo to header and increased margin gap with ml-12

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -9,9 +9,9 @@ export default function HeaderComponent() {
           <nav className='flex justify-between items-center'>
             <div className='flex items-center'>
               <Link to='/' className=''>
-                <img className='w-28 min-w-28 h-auto sm:w-32' src="images/logo2.png" alt="Go to homepage" />
+                <img className='w-28 min-w-28 h-auto sm:w-32' src="images/disney-logo.png" alt="Go to homepage" />
               </Link>
-              <div className='ml-4 hidden md:block'>
+              <div className='ml-12 hidden md:block'>
                 <h1 className='text-2xl font-bold text-blue-700 xl:text-3xl'>Disneyland</h1>
                 <h2 className='font-extralight text-gray-600 text-base'>Anaheim, California</h2>
               </div>
