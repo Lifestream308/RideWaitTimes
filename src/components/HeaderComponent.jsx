fde7b01a55c9eed766054c0d9274f24b1cbbd094
Christian Arroyo <ChristianArroyo308@yahoo.com>
2025-03-22 11:05:36 -0700

    separated logo and location in header. Contact link now looks like others

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -11,15 +11,16 @@ export default function HeaderComponent() {
               <Link to='/' className=''>
                 <img className='w-28 min-w-28 h-auto sm:w-32' src="images/disney-logo.png" alt="Go to homepage" />
               </Link>
-              <div className='ml-12 hidden md:block'>
-                <h1 className='text-2xl font-bold text-blue-700 xl:text-3xl'>Disneyland</h1>
-                <h2 className='font-extralight text-gray-600 text-base'>Anaheim, California</h2>
-              </div>
+            </div>
+            <div className='hidden md:block'>
+              <h1 className='text-2xl font-bold text-blue-700 xl:text-3xl'>Disneyland</h1>
+              <h2 className='font-extralight text-gray-600 text-base'>Anaheim, California</h2>
             </div>
             <ul className='hidden flex-wrap items-center lg:flex'>
               <li><Link to='/' className='px-4 py-2 text-xl font-light text-blue-800 hover:underline'>Home</Link></li>
               <li><Link to='/about' className='px-4 py-2 text-xl font-light text-blue-800 hover:underline' href="">About</Link></li>
-              <li><Link to='/contact' className='px-2 py-1 w-fit text-md font-bold bg-gradient-to-r from-blue-700 to-blue-500 text-white hover:shadow-md hover:text-gray-200 shadow-sm shadow-neutral-600 rounded-lg' href="">Contact Us</Link></li>
+              <li><Link to='/contact' className='px-4 py-2 text-xl font-light text-blue-800 hover:underline' href="">Contact</Link></li>
+              {/* <li><Link to='/contact' className='px-2 py-1 w-fit text-md font-bold bg-gradient-to-r from-blue-700 to-blue-500 text-white hover:shadow-md hover:text-gray-200 shadow-sm shadow-neutral-600 rounded-lg' href="">Contact Us</Link></li> */}
             </ul>
             <HamburgerComponent />
           </nav>
