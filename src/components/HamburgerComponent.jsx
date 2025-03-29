68d6defd693bb2e387dbc8e4230501d4ab45bef5
Christian Arroyo <ChristianArroyo308@yahoo.com>
2025-03-29 00:30:39 -0700

    css changes to hamburger button and menu

--- src/components/HamburgerComponent.jsx
+++ src/components/HamburgerComponent.jsx
@@ -39,12 +39,12 @@ export default function HamburgerComponent() {
   return (
     <>
         <div ref={menuRef} className='flex lg:hidden'>
-            <button className='mr-4 h-20 w-16 relative' onClick={toggleMenu}>
-                <div className={(menuIsOpen? 'hamburgerCrossed' : 'hamburgerStacked') + ' h-2 w-full absolute rounded-lg bg-blue-800 top-1/4 left-1/2'} ></div>
-                <div className={(menuIsOpen? 'hamburgerCrossed' : 'hamburgerStacked') + ' h-2 w-full absolute rounded-lg bg-blue-800 top-1/2 left-1/2'} ></div>
-                <div className={(menuIsOpen? 'hamburgerCrossed' : 'hamburgerStacked') + ' h-2 w-full absolute rounded-lg bg-blue-800 top-3/4 left-1/2'} ></div>
+            <button className='mr-4 h-14 w-14 relative' onClick={toggleMenu}>
+                <div className={(menuIsOpen? 'hamburgerCrossed' : 'hamburgerStacked') + ' h-1 w-4/5 absolute rounded-lg bg-gray-700 top-1/3 left-1/2'} ></div>
+                {/* <div className={(menuIsOpen? 'hamburgerCrossed' : 'hamburgerStacked') + ' h-2 w-4/5 absolute rounded-lg bg-gray-700 top-1/2 left-1/2'} ></div> */}
+                <div className={(menuIsOpen? 'hamburgerCrossed' : 'hamburgerStacked') + ' h-1 w-4/5 absolute rounded-lg bg-gray-700 top-2/3 left-1/2'} ></div>
             </button>
-            <div className={(menuIsOpen? 'w-[12rem] opacity-100' : 'w-0 opacity-0') + ' pb-5 h-fit absolute flex justify-center bg-white top-28 right-0 rounded-bl-lg z-10 transition-all ease-in duration-200 overflow-hidden'}>
+            <div className={(menuIsOpen? 'w-[12rem] opacity-100' : 'w-0 opacity-0') + ' pb-5 h-fit absolute flex justify-center bg-white top-16 right-0 rounded-bl-lg z-10 transition-all ease-in duration-200 overflow-hidden'}>
                 <ul className='pt-6 flex flex-col gap-6 text-center'>
                     <li><Link to='/' className='px-4 py-2 text-2xl font-bold text-blue-800 hover:bg-gray-200 rounded-lg'>Home</Link></li>
                     <li><Link to='/about' className='px-4 py-2 text-2xl font-bold text-blue-800 hover:bg-gray-200 rounded-lg' href="">About</Link></li>
