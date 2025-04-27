17499ccd63bbc216d771cff0c1a2978a737fc4f4
Christian Arroyo <ChristianArroyo308@yahoo.com>
2025-04-27 10:44:26 -0700

    added small explanation of settings page. adjusted margin tops. h2 css now matches h2 of about section

--- src/components/SettingsComponent.jsx
+++ src/components/SettingsComponent.jsx
@@ -22,8 +22,9 @@ export default function SettingsComponent({rideNames, rideFilter, setRideFilter}
   return (
     <div>
         <div className='mt-2 flex flex-col gap-8 px-[6%] bg-slate-100'>
-          <h2 className='text-2xl'>Settings</h2>
-          <button className='px-2 py-1 mt-4 w-fit text-base font-bold bg-gradient-to-r from-blue-700 to-blue-500 text-white hover:shadow-xl hover:text-gray-200 shadow-sm shadow-neutral-600 rounded-lg sm:px-4 sm:py-2 sm:text-xl md:text-2xl sm:mt-6' onClick={handleFilterAll}>Toggle All Rides Off</button>
+          <h2 className='mt-6 text-4xl text-gray-600 font-bold'>Settings</h2>
+          <p className='max-w-[40rem] text-xl text-gray-600'>There's too many rides! Turn all rides off, then click to toggle ON the rides you want to keep track of.</p>
+          <button className='mt-4 px-2 py-1 w-fit text-base font-bold bg-gradient-to-r from-blue-700 to-blue-500 text-white hover:shadow-xl hover:text-gray-200 shadow-sm shadow-neutral-600 rounded-lg sm:px-4 sm:py-2 sm:text-xl md:text-2xl' onClick={handleFilterAll}>Toggle All Rides Off</button>
         {rideNames.map((rideName, index) => (
           <div onClick={() => handleFilter(rideName)} key={index} className='min-h-[3rem] w-full md:w-4/5 max-w-[40rem] grid grid-cols-3 gap-4 border-gray-300 border-b-2'>
             <p className='col-span-2'>{rideName}</p>
