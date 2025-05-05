bb81f29c1a70e00f2cfdaf40f06d6ecd02bc7faa
Christian Arroyo <ChristianArroyo308@yahoo.com>
2025-05-05 11:45:05 -0700

    added new button to turn off all rides. adjusted button colors

--- src/components/SettingsComponent.jsx
+++ src/components/SettingsComponent.jsx
@@ -11,9 +11,17 @@ export default function SettingsComponent({rideNames, rideFilter, setRideFilter}
     }))
   }
 
-  const handleFilterAll = () => {
+  const handleRidesOff = () => {
     let ridesFalseObject = rideNames.reduce((acc, ride) => {
       acc[ride] = true
+      return acc
+    }, {})
+    setRideFilter(ridesFalseObject)
+  }
+
+  const handleRidesOn = () => {
+    let ridesFalseObject = rideNames.reduce((acc, ride) => {
+      acc[ride] = false
       return acc
     }, {})
     setRideFilter(ridesFalseObject)
@@ -24,7 +32,10 @@ export default function SettingsComponent({rideNames, rideFilter, setRideFilter}
         <div className='mt-2 flex flex-col gap-8 px-[6%] bg-slate-100'>
           <h2 className='mt-6 text-4xl text-gray-600 font-bold'>Settings</h2>
           <p className='max-w-[40rem] text-xl text-gray-600'>There's too many rides! Turn all rides off, then click to toggle ON the rides you want to keep track of.</p>
-          <button className='mt-4 px-2 py-1 w-fit text-base font-bold bg-gradient-to-r from-blue-700 to-blue-500 text-white hover:shadow-xl hover:text-gray-200 shadow-sm shadow-neutral-600 rounded-lg sm:px-4 sm:py-2 sm:text-xl md:text-2xl' onClick={handleFilterAll}>Toggle All Rides Off</button>
+          <div className='flex gap-12'>
+            <button className='mt-4 px-2 py-1 w-fit text-base font-bold bg-gradient-to-r from-purple-700 to-red-600 text-white hover:shadow-xl hover:text-gray-200 shadow-sm shadow-neutral-600 rounded-lg sm:px-4 sm:py-2 sm:text-xl md:text-2xl' onClick={handleRidesOff}>All Rides Off</button>
+            <button className='mt-4 px-2 py-1 w-fit text-base font-bold bg-gradient-to-r from-purple-700 to-blue-600 text-white hover:shadow-xl hover:text-gray-200 shadow-sm shadow-neutral-600 rounded-lg sm:px-4 sm:py-2 sm:text-xl md:text-2xl' onClick={handleRidesOn}>All Rides On</button>
+          </div>
         {rideNames.map((rideName, index) => (
           <div onClick={() => handleFilter(rideName)} key={index} className='min-h-[3rem] w-full md:w-4/5 max-w-[40rem] grid grid-cols-3 gap-4 border-gray-300 border-b-2'>
             <p className='col-span-2'>{rideName}</p>
