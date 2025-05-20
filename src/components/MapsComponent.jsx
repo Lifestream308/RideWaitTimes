781a3a1c808775688fb0f5888b4c823a9fb87f10
Christian <ChristianArroyo308@yahoo.com>
2025-05-19 22:11:10 -0700

    creating temporary button to test API results

--- src/components/MapsComponent.jsx
+++ src/components/MapsComponent.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
 
 export default function MapsComponent() {
+
+  async function testAPI() {
+    
+      try {
+        const response = await fetch('https://api.themeparks.wiki/v1/entity/7340550b-c14d-4def-80bb-acdb51d49a66/live');
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log('Data received:', data);
+      } catch (error) {
+        console.error('Fetch error:', error.message);
+      }
+  }
+
+
   return (
     <>
       <div className='mt-2 flex flex-col gap-6 lg:gap-16 px-[6%] text-blue-900 bg-slate-100'>
@@ -8,6 +26,7 @@ export default function MapsComponent() {
           <div className='flex flex-col gap-4'>
             <h2 className='text-gray-600 text-4xl font-bold'>Maps</h2>
             <p>Need to add google maps link to different Disney locations like parking structure Mickey and Friends and maybe one other. Also link to Map of parks.</p>
+            <button className='w-fit p-2 text-xl text-white rounded-lg bg-blue-700' onClick={testAPI}>Test Button</button>
           </div>
         </div>
       </div>
