74fb5fd19d9c6bb183964831f8ca49899a9d3075
Christian Arroyo <ChristianArroyo308@yahoo.com>
2025-06-09 00:41:57 -0700

    updated text. More concise and clear explanation of maps

--- src/components/MapsComponent.jsx
+++ src/components/MapsComponent.jsx
@@ -32,14 +32,15 @@ export default function MapsComponent() {
         <div className='mt-6 max-w-[50rem] flex flex-col gap-12 text-2xl text-gray-500'>
           <div className='flex flex-col gap-4'>
             <h2 className='text-gray-600 text-4xl font-bold'>Maps</h2>
-            <p>Need to add google maps link to different Disney locations like parking structure Mickey and Friends and maybe one other. Also link to Map of parks.</p>
+            <h3 className='mt-4'>Google maps</h3>
             <a className='w-fit p-2 text-xl text-white bg-blue-700 rounded-lg' target='_blank' href='https://www.google.com/maps/dir//1313+Disneyland+Dr,+Anaheim,+CA+92802/@33.8145851,-117.9217141,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x80dcd7d400f5ab31:0x1bcbcc05a3d825!2m2!1d-117.9191392!2d33.8145851!3e0?entry=ttu&g_ep=EgoyMDI1MDUyOC4wIKXMDSoASAFQAw%3D%3D'>
-              Google Maps Link to Mickey And Friends Parking Structure
+              Mickey And Friends Parking Structure
             </a>
-            <a className='w-fit p-2 text-xl text-white bg-blue-700 rounded-lg' target='_blank' href='https://www.dreamsunlimitedtravel.com/disneyland/disneyland-map.htm'>
+            <h3 className='mt-4'>Picture maps</h3>
+            <a className='mb-4 w-fit p-2 text-xl text-white bg-blue-700 rounded-lg' target='_blank' href='https://www.dreamsunlimitedtravel.com/disneyland/disneyland-map.htm'>
               Maps of Disneyland
             </a>
-            <button className='w-fit p-2 text-xl text-white rounded-lg bg-blue-700' onClick={testAPI}>Test Button</button>
+            {/* <button className='w-fit p-2 text-xl text-white rounded-lg bg-blue-700' onClick={testAPI}>Test Button</button> */}
           </div>
         </div>
       </div>
