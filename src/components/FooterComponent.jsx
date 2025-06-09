3c8dc7b7daa6210ddb6b2660844ded64b50e5869
Christian Arroyo <ChristianArroyo308@yahoo.com>
2025-06-09 00:32:18 -0700

    changed footer text. Will eventually place real email/name

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -4,8 +4,8 @@ export default function FooterComponent() {
   return (
     <>
         <footer className='flex flex-col mt-14 mb-16 gap-8 text-blue-900 text-center'>
-            <h3>SetCompanyEmail@gmail.com</h3>
-            <small>©{new Date().getFullYear()} by Set Company Name</small>
+            <h3>Email@gmail.com</h3>
+            <small>©{new Date().getFullYear()} by CA</small>
         </footer>
     </>
   )
